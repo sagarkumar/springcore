0331c37c01624816193465998c6a6fcd3f92588d
Sagarkumar Prajapati <sagarp72@gmail.com>
2023-11-30 18:17:45 +0530

    Initial commit

---
+++ src/main/java/org/sagarpr/MyConfig.java
@@ -0,0 +1,15 @@
+package org.sagarpr;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(value = "org.sagarpr")
+public class MyConfig {
+
+//    @Bean
+//    public Person getPerson(){
+//        return new Person(45,"Sagar");
+//    }
+}
